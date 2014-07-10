37b5b4d732a1421fc1edecf208f1c948be1bbd76
Darryl Cousins <darryljcousins@gmail.com>
2014-07-10 16:29:32 +1200

    working up test app and other tweaks

---
+++ test_django_admin_bootstrapped/test_django_admin_bootstrapped/development.py
@@ -0,0 +1,18 @@
+from .settings import *
+
+INSTALLED_APPS = (
+    'django_admin_bootstrapped',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'bootstrap3',
+    'test_django_admin_bootstrapped',
+    'CapitalApp',
+)
+
+AUTOCOMPLETE_MODELS = []
+
